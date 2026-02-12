23e89659442503ed6af2b5d9f577b15293825c5a
Elias Alves <helias.esa@outlook.com>
2026-02-12 19:54:03 -0300

    Novo código

--- config.php
+++ config.php
@@ -1,14 +1,8 @@
 <?php
 
-// BANCO
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "sav");
+$omada_url = "https://10.5.0.20:8043";
+$omada_user = "SEU_USUARIO";
+$omada_pass = "SUA_SENHA";
+$site = "hrvj"; // nome do site no omada
 
-// OMADA
-define("OMADA_URL", "https://10.5.0.20:8043");
-define("OMADA_ID", "0a98965885f4db539c6d058a3edf47e5");
-define("CLIENT_ID", "859ee53945344ca0a61f3feb9f6cf0e2");
-define("CLIENT_SECRET", "72efa96f5bde4f0babc3edf31db6a777");
-define("SITE", "HRVJ");
+?>
--- omada.php
+++ omada.php
@@ -1,41 +1,25 @@
 <?php
-require_once "config.php";
 
-function autenticarCliente($mac){
+function omadaLogin($url, $user, $pass) {
 
-    // Obter token
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, OMADA_URL."/openapi/authorize/token");
+    curl_setopt($ch, CURLOPT_URL, "$url/api/v2/login");
     curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,
+        json_encode([
+            "username" => $user,
+            "password" => $pass
+        ])
+    );
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
-        "grant_type" => "client_credentials",
-        "client_id" => CLIENT_ID,
-        "client_secret" => CLIENT_SECRET
-    ]));
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-    $result = curl_exec($ch);
-    $json = json_decode($result,true);
-
-    $token = $json["access_token"];
-
-    curl_close($ch);
-
-    // Autorizar cliente
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, OMADA_URL."/openapi/v1/".$GLOBALS['SITE']."/portal/auth");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Authorization: Bearer ".$token,
-        "Content-Type: application/json"
+        'Content-Type: application/json'
     ]);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
-        "mac" => $mac
-    ]));
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-    curl_exec($ch);
+    $response = curl_exec($ch);
     curl_close($ch);
+
+    return $response;
 }
+?>
